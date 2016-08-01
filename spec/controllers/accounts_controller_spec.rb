71ed4c94199b074c0ced819bd720dac0f8554aa9
kortirso <kortirso@gmail.com>
2016-08-01 14:33:26 +0300

    add spec for accounts#index

--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -1,3 +1,28 @@
 RSpec.describe AccountsController, type: :controller do
+    describe 'GET #index' do
+        it 'redirect to welcome page if user not login' do
+            get :index
 
+            expect(response).to render_template 'welcome/index'
+        end
+
+        context 'When user logged in' do
+            sign_in_user
+            let!(:cards) { create_list(:card, 2) }
+            let!(:pack) { create :pack, user: @current_user, card: cards.first }
+            before { get :index }
+
+            it 'redirects to account page' do
+                expect(response).to render_template :index
+            end
+
+            it 'collects an array of cards' do
+                expect(assigns(:cards)).to match_array(cards.to_a)
+            end
+
+            it 'collects an array of packs with card ids' do
+                expect(assigns(:packs)).to match_array([pack.card_id])
+            end
+        end
+    end
 end

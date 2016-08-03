451557207a94a132da37370f82f8f119d9a4868a
kortirso <kortirso@gmail.com>
2016-08-03 14:51:08 +0300

    user can change cards in collection from 0 to 2

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,6 +1,22 @@
 $(function() {
     $('.costs label').click(function(e) {
-        if($(this).hasClass('checked')) $(this).removeClass('checked');
-        else $(this).addClass('checked');
+        if($(this).hasClass('none')) {
+            $(this).removeClass('none');
+            $(this).addClass('single');
+            $(this).closest('p').children('input').val(1);
+            $(this).closest('p').children('input').prop('checked', false);
+        }
+        else if($(this).hasClass('single')) {
+            $(this).removeClass('single');
+            $(this).addClass('double');
+            $(this).closest('p').children('input').val(2);
+            $(this).closest('p').children('input').prop('checked', false);
+        }
+        else {
+            $(this).removeClass('double');
+            $(this).addClass('none');
+            $(this).closest('p').children('input').val(0);
+            $(this).closest('p').children('input').prop('checked', true);
+        }
     });
 });
--- app/models/pack.rb
+++ app/models/pack.rb
@@ -7,15 +7,24 @@ class Pack < ApplicationRecord
 
     def self.collect_ids
         ids = []
-        all.each { |pack| ids.push pack.card_id }
+        all.each { |pack| ids.push [pack.card_id, pack.amount] }
         return ids
     end
 
     def self.build(user_id, params, old_ids)
         packs = []
-        ids = params.to_a.collect { |elem| elem[0] }.delete_if { |elem| elem == 'utf8' || elem == 'commit' }.map { |elem| elem.to_i }
-        old_ids.each { |old_id| Pack.find_by(card_id: old_id, user_id: user_id).destroy unless ids.include?(old_id) }
-        ids.each { |id| packs << Pack.new(card_id: id, user_id: user_id, amount: 1) unless old_ids.include?(id) }
+        ids = params.to_a.delete_if { |elem| elem[0] == 'utf8' || elem[0] == 'commit' }.map { |elem| elem.map { |x| x.to_i } }
+        ids_only_id = ids.collect { |i| i[0] }
+        old_ids_only_id = old_ids.collect { |i| i[0] }
+        old_ids.each do |old_id|
+            if ids_only_id.include?(old_id[0])
+                new_amount = ids[ids_only_id.index(old_id[0])][1]
+                Pack.find_by(card_id: old_id[0], user_id: user_id).update(amount: new_amount) if old_id[1] != new_amount
+            else
+                Pack.find_by(card_id: old_id[0], user_id: user_id).destroy
+            end
+        end
+        ids.each { |id| packs << Pack.new(card_id: id[0], user_id: user_id, amount: id[1]) unless old_ids_only_id.include?(id[0]) }
         Pack.import packs
     end
 end

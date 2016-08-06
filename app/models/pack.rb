f4b53e731571deaeded13a333d6021730164a043
kortirso <kortirso@gmail.com>
2016-08-06 09:41:35 +0300

    fix Pack.build method

--- app/models/pack.rb
+++ app/models/pack.rb
@@ -11,20 +11,20 @@ class Pack < ApplicationRecord
         return ids
     end
 
-    def self.build(user_id, params, old_ids)
-        packs = []
-        ids = params.to_a.delete_if { |elem| elem[0] == 'utf8' || elem[0] == 'commit' }.map { |elem| elem.map { |x| x.to_i } }
-        ids_only_id = ids.collect { |i| i[0] }
-        old_ids_only_id = old_ids.collect { |i| i[0] }
+    def self.build(user_id, params, old)
+        cards = Deck.remove_params(params).map { |elem| elem.map { |x| x.to_i } }
+        cards_ids = cards.collect { |i| i[0] }
+        old_ids = old.collect { |i| i[0] }
         old_ids.each do |old_id|
-            if ids_only_id.include?(old_id[0])
-                new_amount = ids[ids_only_id.index(old_id[0])][1]
-                Pack.find_by(card_id: old_id[0], user_id: user_id).update(amount: new_amount) if old_id[1] != new_amount
+            if cards_ids.include?(old_id)
+                new_amount = cards[cards_ids.index(old_id)][1]
+                Pack.find_by(card_id: old_id, user_id: user_id).update(amount: new_amount) if old[old_ids.index(old_id)][1] != new_amount
             else
-                Pack.find_by(card_id: old_id[0], user_id: user_id).destroy
+                Pack.find_by(card_id: old_id, user_id: user_id).destroy
             end
         end
-        ids.each { |id| packs << Pack.new(card_id: id[0], user_id: user_id, amount: id[1]) unless old_ids_only_id.include?(id[0]) }
+        packs = []
+        cards_ids.each { |id| packs << Pack.new(card_id: id, user_id: user_id, amount: cards[cards_ids.index(id)][1]) unless old_ids.include?(id) }
         Pack.import packs
     end
 end

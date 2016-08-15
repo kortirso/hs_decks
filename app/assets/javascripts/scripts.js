8f93ced9557ead7ac07b23b82afefe79e60e1a01
kortirso <kortirso@gmail.com>
2016-08-15 12:08:46 +0300

    add cards mana_cost and amount indicators

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -136,20 +136,20 @@ $(function() {
     $('#free_existed').on('click', function(e) {
         e.preventDefault();
         $('.card.Free input').val(2).prop('checked', true);
-        $('.card.Free label').removeClass('none').removeClass('single').addClass('double');
+        $('.card.Free label').removeClass('none').removeClass('single').removeClass('double').addClass('double');
     });
 
     $('.class_existed').on('click', function(e) {
         e.preventDefault();
         name = $(this).attr('class').split(' ').pop()
         $('.' + name + ' input').val(2).prop('checked', true);
-        $('.' + name + ' label').removeClass('none').removeClass('single').addClass('double');
+        $('.' + name + ' label').removeClass('none').removeClass('single').removeClass('double').addClass('double');
     });
 
     $('.class_unexisted').on('click', function(e) {
         e.preventDefault();
         name = $(this).attr('class').split(' ').pop();
         $('.' + name + ' input').val(0);
-        $('.' + name + ' label').removeClass('double').removeClass('single').addClass('none');
+        $('.' + name + ' label').removeClass('none').removeClass('single').removeClass('double').addClass('none');
     });
 });

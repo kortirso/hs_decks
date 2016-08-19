00a03d76024349d047a13a07e5209022deeb2a3f
kortirso <kortirso@gmail.com>
2016-08-19 20:30:18 +0300

    add services specs

--- spec/models/check_spec.rb
+++ spec/models/check_spec.rb
@@ -73,22 +73,6 @@ RSpec.describe Check, type: :model do
             end
         end
 
-        context '.check_getting_params' do
-            let(:params) { Parametrize.check_getting_params(empty_params) }
-
-            it 'should returns hash with 4 params' do
-                expect(params.kind_of? Hash).to eq true
-                expect(params.size).to eq 4
-            end
-
-            it 'and all 4 params are not nil' do
-                expect(params['success']).to_not eq nil
-                expect(params['dust']).to_not eq nil
-                expect(params['playerClass']).to_not eq nil
-                expect(params['formats']).to_not eq nil
-            end
-        end
-
         context '.getting_decks' do
             let!(:shaman_deck) { create :deck, playerClass: 'Shaman', formats: 'wild' }
             let!(:hunter_deck) { create :deck, playerClass: 'Hunter', formats: 'wild' }
---
+++ spec/services/dust_price_spec.rb
@@ -0,0 +1,23 @@
+describe DustPrice do
+    context '.calc' do
+        it 'should returns 0 if card rarity is Free' do
+            expect(DustPrice.calc('Free', 1)).to eq 0
+        end
+
+        it 'should returns 40 if card rarity is Common' do
+            expect(DustPrice.calc('Common', 1)).to eq 40
+        end
+
+        it 'should returns 100 if card rarity is Rare' do
+            expect(DustPrice.calc('Rare', 1)).to eq 100
+        end
+
+        it 'should returns 400 if card rarity is Epic' do
+            expect(DustPrice.calc('Epic', 1)).to eq 400
+        end
+
+        it 'should returns 1600 if card rarity is Legendary' do
+            expect(DustPrice.calc('Legendary', 1)).to eq 1600
+        end
+    end
+end

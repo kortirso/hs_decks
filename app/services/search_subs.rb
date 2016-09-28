2aeddab87626b4cc593c34a139c6cf09d62c26aa
kortirso <kortirso@gmail.com>
2016-09-28 15:20:48 +0300

    modify SearchSubs service

--- app/services/search_subs.rb
+++ app/services/search_subs.rb
@@ -6,15 +6,35 @@ class SearchSubs
     end
 
     def find_exchange(card_id, amount, pos_ids, subs_ids)
+        return result unless (result = check_shifts_in_deck).nil? # 1. todo: Looking for substitutions in decks shifts
+        return result unless (result = check_shifts_in_class).nil? # 2. todo: Looking for substitutions in other class decks
+        return result unless (result = check_all_shifts(card_id, pos_ids, subs_ids, amount)).nil? # 3. Looking for substitutions in all shifts
+        check_random_shifts(card_id, pos_ids, subs_ids, amount) # 4. Looking last chance substitutions
+    end
+
+    private
+
+    def check_shifts_in_deck
+        nil
+    end
+
+    def check_shifts_in_class
+        nil
+    end
+
+    def check_all_shifts(card_id, pos_ids, subs_ids, amount)
         card_for_sub = Card.find_by(id: card_id)
         card_for_sub.shifts.order(priority: :desc).each do |shift|
             return [shift.change.id, amount] if is_sub_approached?(shift, pos_ids, subs_ids)
         end
+        nil
+    end
+
+    def check_random_shifts(card_id, pos_ids, subs_ids, amount)
+        card_for_sub = Card.find_by(id: card_id)
         cards_for_random = @cards.select { |card| card.cost == card_for_sub.cost }.delete_if { |card| is_card_in_deck?(card, pos_ids, subs_ids, card_id) }
         cards_for_random.size > 0 ? [cards_for_random.sample.id, amount] : [card_id, amount]
     end
-
-    private
 
     def is_sub_approached?(shift, pos_ids, subs_ids)
         return false unless shift.change.playerClass.nil? || shift.change.playerClass == @playerClass

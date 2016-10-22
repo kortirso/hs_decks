8633a13c8997d8db25bfeb2b65bc783c81e6e089
kortirso <kortirso@gmail.com>
2016-10-22 12:55:04 +0300

    fix updating collection

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,28 +25,29 @@ class User < ApplicationRecord
         if cards.size > 0
             cards_ids = cards.collect { |i| i[0] }
             old_ids = old.collect { |i| i[0] }
-            self.update_collection(old_ids, cards_ids, cards, old, params[0][0][1].to_i) unless params[0][0].nil?
+            self.update_collection(old_ids, cards_ids, cards, old)
             self.adding_to_collection(old_ids, cards_ids, cards)
-            ## todo removing from collection if amount -> 0
         end
     end
 
-    def update_collection(old_ids, cards_ids, cards, old, direction)
+    def update_collection(old_ids, cards_ids, cards, old)
         positions = self.positions
         old_ids.each do |old_id|
             if cards_ids.include?(old_id)
                 new_amount = cards[cards_ids.index(old_id)][1]
-                positions.find_by(card_id: old_id).update(amount: new_amount) if old[old_ids.index(old_id)][1] != new_amount
-            elsif direction == 0
-                positions.find_by(card_id: old_id).destroy
+                if new_amount.zero?
+                    positions.find_by(card_id: old_id).destroy
+                else
+                    positions.find_by(card_id: old_id).update(amount: new_amount) if old[old_ids.index(old_id)][1] != new_amount
+                end
             end
         end
     end
 
     def adding_to_collection(old_ids, cards_ids, cards)
-        positions, t = [], Time.current
-        cards_ids.each { |id| positions.push "(#{id}, '#{self.id}', 'User', '#{cards[cards_ids.index(id)][1]}', '#{t}', '#{t}')" unless old_ids.include?(id) }
-        Position.connection.execute "INSERT INTO positions (card_id, positionable_id, positionable_type, amount, created_at, updated_at) VALUES #{positions.join(", ")}" if positions.size > 0
+        pos, t = [], Time.current
+        cards_ids.each { |id| pos.push "(#{id}, '#{self.id}', 'User', '#{cards[cards_ids.index(id)][1]}', '#{t}', '#{t}')" if !old_ids.include?(id) && cards[cards_ids.index(id)][1] != 0 }
+        Position.connection.execute "INSERT INTO positions (card_id, positionable_id, positionable_type, amount, created_at, updated_at) VALUES #{pos.join(", ")}" if pos.size > 0
     end
 
     def get_unusable_cards

67f243accd05c66d3bc9760e26ba90f6cb349f8e
kortirso <kortirso@gmail.com>
2017-03-05 19:29:35 +0700

    put short cards in asset pipeline

---
+++ app/services/cards_styles_compiler_service.rb
@@ -0,0 +1,9 @@
+class CardsStylesCompilerService
+    def self.call
+        open("#{Rails.root}/app/assets/stylesheets/short_cards.scss", 'w') do |f|
+            f.puts '.costs .card .image, .check .card .image, #cards_list .card .image, #exchanges .card .image, #lines .card .image {'
+            Card.not_heroes.order(cardId: :asc).each { |card| f.puts "&.card_#{card.cardId}{background-image: image-url('short_cards/#{card.cardId}_short.jpg')}" }
+            f.puts '}'
+        end
+    end
+end

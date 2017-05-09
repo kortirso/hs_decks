25203f060f649965a6d75e9667932e663d8e1257
kortirso <kortirso@gmail.com>
2017-05-09 06:07:12 +0300

    fix sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,16 +1,9 @@
 require 'sidekiq'
-require 'sidekiq-status'
 
 Sidekiq.configure_client do |config|
-    config.client_middleware do |chain|
-        chain.add Sidekiq::Status::ClientMiddleware
-    end
     config.redis = { namespace: 'hs_decks', url: 'redis://127.0.0.1:6379/0' }
 end
 
 Sidekiq.configure_server do |config|
-    config.server_middleware do |chain|
-        chain.add Sidekiq::Status::ServerMiddleware, expiration: 30.minutes
-    end
     config.redis = { namespace: 'hs_decks', url: 'redis://127.0.0.1:6379/0' }
 end

1b117050d1658084fa34cb72e3ed2815f26a9284
kortirso <kortirso@gmail.com>
2017-11-11 15:18:16 +0200

    drop useless tables

---
+++ db/migrate/20171111131700_drop_tables.rb
@@ -0,0 +1,7 @@
+class DropTables < ActiveRecord::Migration[5.1]
+    def change
+        drop_table :oauth_access_grants
+        drop_table :oauth_access_tokens
+        drop_table :oauth_applications
+    end
+end

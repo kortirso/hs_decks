54764f6e96c6581407c0dbbabe4796312f6b2e87
kortirso <kortirso@gmail.com>
2017-12-01 23:05:35 +0300

    add scraping service for decks from tempostorm

---
+++ app/services/tempo_scrape_service.rb
@@ -0,0 +1,33 @@
+require 'watir'
+
+class TempoScrapeService
+    attr_reader :browser, :user_id
+
+    def initialize
+        @browser = Watir::Browser.new :chrome, headless: true
+        @user_id = User.find_by(email: 'kortirso@gmail.com').id
+    end
+
+    def perform(filename)
+        decks_file = File.read("#{Rails.root}/db/scrapes/#{filename}")
+        JSON.parse(decks_file).each { |deck| create_deck(deck) }
+    end
+
+    private
+
+    def create_deck(deck)
+        browser.goto(deck['url'])
+
+        Watir::Wait.until { browser.div(class: 'db-deck-cards').exists? }
+
+        deck = Deck.create name: deck['name'], name_en: deck['name_en'], playerClass: deck['player_class'], formats: deck['formats'], link: deck['url'], user_id: user_id, player_id: Player.find_by(name_en: deck['player_class']).id, power: deck['power'], author: deck['author']
+
+        browser.elements(class: 'db-deck-card').each do |elem|
+            card_name = elem.div(class: 'db-deck-card-name').text
+            amount = elem.div(class: 'db-deck-card-qty').present? ? elem.div(class: 'db-deck-card-qty').text : 1
+            deck.positions.create(amount: amount, card_id: Card.find_by(name_en: card_name).id)
+        end
+
+        sleep(1)
+    end
+end

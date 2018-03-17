e078dec26a3ad50407fd9cd7bcc177ee7a4a4782
kortirso <kortirso@gmail.com>
2018-03-17 14:47:33 +0700

    add component deck

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -10,6 +10,7 @@ import "components/footer/footer";
 import "components/index/index";
 import "components/collection/collection";
 import "components/decks/decks";
+import "components/deck/deck";
 
 import "components/admin_decks/admin_decks";
 import "components/admin_decks_new/admin_decks_new";
